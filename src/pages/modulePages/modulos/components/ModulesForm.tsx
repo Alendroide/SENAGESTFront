c8ae9e29137a18473b626e34a5b4f91d91c6d650
Alendroide <alendroideyt@gmail.com>
2025-04-23 08:50:44 -0500

    Icon is now displayed in the Input Selector for module creation

--- src/pages/modulePages/modulos/components/ModulesForm.tsx
+++ src/pages/modulePages/modulos/components/ModulesForm.tsx
@@ -1,7 +1,8 @@
 import ErrorMessage from "@/components/atoms/text/ErrorMessage";
+import { iconsConfig } from "@/config/icons";
 import useModule from "@/hooks/default/useModule";
 import { ModuleSchema } from "@/types/modules/Module";
-import { Button, Form, Input } from "@heroui/react";
+import { Button, Form, Input, Select, SelectItem } from "@heroui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
@@ -15,12 +16,23 @@ export default function ModulesForm(){
 
     return(
         <Form onSubmit={handleSubmit(createModule)}>
+
             <Input {...register("nombre")} label='Nombre' />
             {errors.nombre && <ErrorMessage>{errors.nombre.message}</ErrorMessage>}
+
             <Input {...register("descripcion")} label='Descripcion' />
             {errors.descripcion && <ErrorMessage>{errors.descripcion.message}</ErrorMessage>}
-            <Input {...register("icono")} label="Icono" />
+
+            <Select {...register("icono")} label="Icono">
+                {Object.entries(iconsConfig).map(([key,Icon]) => (
+                    <SelectItem key={key}>
+                        {Icon}{key}
+                    </SelectItem>
+                ))}
+            </Select>
+
             {errors.icono && <ErrorMessage>{errors.icono.message}</ErrorMessage>}
+
             <Button type="submit" color="success" variant="bordered">Crear</Button>
         </Form>
     )
