6f95f6e39a101f3efc9130fc9d681a9200e1ebc8
Alendroide <alendroideyt@gmail.com>
2025-05-30 15:46:24 -0500

    image added to usuarios table

--- src/pages/modulePages/usuarios/components/UsuariosTable.tsx
+++ src/pages/modulePages/usuarios/components/UsuariosTable.tsx
@@ -1,5 +1,5 @@
 import useUsuario from "@/hooks/default/useUsuario";
-import { Usuario } from "@/types/modules/Usuario";
+import { Usuario as IncompleteUsuario } from "@/types/modules/Usuario";
 import {
   Pagination,
   Table,
@@ -10,6 +10,8 @@ import {
   TableRow,
 } from "@heroui/react";
 
+type Usuario = IncompleteUsuario & {id: number, img: string};
+
 export default function UsuariosTable() {
   const { users, isLoading, isError, error, setPage, totalPages } =
     useUsuario();
@@ -18,31 +20,38 @@ export default function UsuariosTable() {
     <>
       <Table aria-label="UsuariosTable">
         <TableHeader>
+          <TableColumn>Foto</TableColumn>
           <TableColumn>Identificación</TableColumn>
           <TableColumn>Nombre completo</TableColumn>
           <TableColumn>Correo</TableColumn>
+          <TableColumn>Ficha</TableColumn>
         </TableHeader>
         <TableBody>
           {isLoading && (
             <TableRow>
-              <TableCell colSpan={3}>Cargando...</TableCell>
+              <TableCell colSpan={5}>Cargando...</TableCell>
             </TableRow>
           )}
           {isError && (
             <TableRow>
-              <TableCell colSpan={3}>Error: {error?.message}</TableCell>
+              <TableCell colSpan={5}>Error: {error?.message}</TableCell>
             </TableRow>
           )}
-          {users?.map((user: Usuario) => (
-            <TableRow key={user.identificacion}>
-              <TableCell>{user.identificacion}</TableCell>
-              <TableCell>
-                {user.primerNombre} {user.segundoNombre ?? ""}{" "}
-                {user.primerApellido} {user.segundoApellido ?? ""}
-              </TableCell>
-              <TableCell>{user.correo}</TableCell>
-            </TableRow>
-          ))}
+          {users?.map((user: Usuario) => {
+            const apiURL = import.meta.env.VITE_API_URL
+            return(
+              <TableRow key={user.id}>
+                <TableCell><img src={`${apiURL}uploads/${user.img}`} alt="Foto de perfil" className="w-10 h-10 rounded-full" /></TableCell>
+                <TableCell>{user.identificacion}</TableCell>
+                <TableCell>
+                  {user.primerNombre} {user.segundoNombre ?? ""}{" "}
+                  {user.primerApellido} {user.segundoApellido ?? ""}
+                </TableCell>
+                <TableCell>{user.correo}</TableCell>
+                <TableCell>{user.fichaId}</TableCell>
+              </TableRow>
+          )})}
+          
         </TableBody>
       </Table>
       <Pagination
