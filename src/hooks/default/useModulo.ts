4a8663ab6075dd21431598c906e93c5be80ae97e
Alendroide <alendroideyt@gmail.com>
2025-06-01 17:33:24 -0500

    useModulo queryClient data updated in-time

--- src/hooks/default/useModulo.ts
+++ src/hooks/default/useModulo.ts
@@ -1,11 +1,13 @@
 import { axiosAPI } from "@/api/axiosAPI";
 import { Module } from "@/types/modules/Module";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function useModulo() {
+
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const queryClient = useQueryClient();
 
   async function getModules() {
     try {
@@ -30,16 +32,30 @@ export default function useModulo() {
   async function createModule(data: Module) {
     try {
       const response = await axiosAPI.post("modulos", data);
-      return response.data.data;
+      const newRecord = response.data.data;
+      queryClient.invalidateQueries({
+        queryKey: ["modules"],
+        exact: false
+      });
+      return newRecord;
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function updateModule(id:number, data: Module) {
+  async function updateModule(id:number, data: Module & { id: number }) {
     try {
       const response = await axiosAPI.patch(`modulos/update/${id}`, data);
-      return response.data.data;
+      const updatedRecord = response.data.data;
+      queryClient.setQueryData(["modules", page],(oldData: (Module & { id: number })[]) =>
+        oldData.map((module) => {
+          if (module.id === id) {
+            return updatedRecord;
+          }
+          return module;
+        })
+      )
+      return updatedRecord;
     } catch (error) {
       console.log(error);
     }
