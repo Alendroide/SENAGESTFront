1fbe2e2aee10fe4321bd5463e2a58a52ee87df32
Alendroide <alendroideyt@gmail.com>
2025-06-01 00:07:12 -0500

    PageTitle bottom margin added

--- src/components/atoms/PageTitle.tsx
+++ src/components/atoms/PageTitle.tsx
@@ -1,5 +1,5 @@
 export default function PageTitle({children} : {children: React.ReactNode}){
     return(
-        <h1 className="text-2xl font-bold mt-6">{children}</h1>
+        <h1 className="text-2xl font-bold my-6">{children}</h1>
     )
 }
