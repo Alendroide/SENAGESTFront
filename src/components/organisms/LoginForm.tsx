b6d56518fa7d5f64571fd1888d1b571225d06bfa
Alendroide <alendroideyt@gmail.com>
2025-05-31 21:49:56 -0500

    Image added to LoginPage

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -2,13 +2,13 @@
 //Components
 import { Input } from '@heroui/input';
 import { Button } from '@heroui/button';
-import Title from '../atoms/text/Title';
 
 //FormLogic
 import { useForm } from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 import { Login, LoginSchema } from '@/types/default/Login';
 import useAuth from '@/hooks/auth/useAuth';
+import { Link } from 'react-router-dom';
 
 export default function LoginForm(){
 
@@ -16,21 +16,27 @@ export default function LoginForm(){
     const { handleSubmit, formState : { errors }, register } = useForm<Login>({resolver : zodResolver(LoginSchema)});
 
     return(
-        <div className='md:w-1/2 mx-auto'>
-            <Title>Log In</Title>
+        <div className='w-11/12 bg-white box-border p-12 rounded-xl border-1 border-solid border-gray-300 mx-auto flex flex-col md:w-2/3'>
+            <p className='text-gray-400'>Introduce tus credenciales</p>
+            <h1 className='text-3xl font-bold mt-2 mb-8 text-gray-800'>Bienvenido!</h1>
             <form onSubmit={handleSubmit(login)} className='space-y-4'>
                 <Input
                     {...register('correo')}
-                    label="Correo"
+                    label="Correo electrónico"
                 />
                 {errors?.correo && <p className='text-red-600'>{errors.correo.message}</p>}
                 <Input
                     {...register('contrasena')}
                     label="Contraseña"
                     type='password'
-                    />
+                />
                 {errors?.contrasena && <p className='text-red-600'>{errors.contrasena.message}</p>}
-                <Button type='submit' color='success' variant='bordered'>Log In</Button>
+                <div className='flex justify-end'>
+                    <Link to={'/forgot-password'} className='text-blue-600 my-2 font-light'>Olvidaste tu contraseña?</Link>
+                </div>
+                <div>
+                    <Button type='submit' className='bg-blue-900 text-white text-md font-light w-full'>Iniciar sesión</Button>
+                </div>
             </form>
         </div>
     )
--- src/pages/defaultPages/LoginPage.tsx
+++ src/pages/defaultPages/LoginPage.tsx
@@ -1,9 +1,23 @@
 import LoginForm from "@/components/organisms/LoginForm";
 
 export default function LoginPage() {
-    return(
-        <div className="min-h-screen bg-gray-100">
-            <LoginForm/>
-        </div>
-    )
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <img
+        src="/logodark.png"
+        alt="logoSenagest"
+        className="h-20 pt-6 ps-6 sm:h-24"
+      />
+      <div className="absolute top-0 left-0 h-screen w-full xl:w-1/2 flex items-center justify-center">
+        <LoginForm />
+      </div>
+      <div className="hidden absolute top-0 right-0 h-screen xl:w-1/2 xl:flex overflow-hidden bg-blue-900">
+        <img
+          src="/loginbackground.jpg"
+          alt="loginbackground"
+          className="w-full h-full object-cover object-center opacity-80"
+        />
+      </div>
+    </div>
+  );
 }
