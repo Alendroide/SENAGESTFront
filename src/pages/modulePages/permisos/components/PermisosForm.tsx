6bf390d5a4ddd9952386a22b888141ec8be1806e
Alendroide <alendroideyt@gmail.com>
2025-06-03 08:30:47 -0500

    PermisosForm updated to current DTO

--- src/pages/modulePages/permisos/components/PermisosForm.tsx
+++ src/pages/modulePages/permisos/components/PermisosForm.tsx
@@ -1,25 +1,27 @@
 import ErrorMessage from "@/components/atoms/text/ErrorMessage";
 import { iconsConfig, typeIcons } from "@/config/icons";
-import useModule from "@/hooks/default/useModulo";
 import usePermiso from "@/hooks/default/usePermiso";
 import { Module } from "@/types/modules/Module";
 import { Permiso, PermisoSchema } from "@/types/modules/Permiso";
-import { Button, Divider, Form, Input, Select, SelectItem, Textarea } from "@heroui/react";
+import { Button, Form, Input, Select, SelectItem, Textarea } from "@heroui/react";
 import { useModalContext } from "@heroui/modal";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export default function PermisosForm() {
 
-    const { modules } = useModule();
-
-    const { createPermiso } = usePermiso();
-
-    const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm({
-        resolver: zodResolver(PermisoSchema)
-    })
+    const { createPermiso, modules } = usePermiso();
+    const [ selectedModule, setSelectedModule ] = useState<number | null>(null);
 
     const {onClose} = useModalContext();
+
+    const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm({
+        resolver: zodResolver(PermisoSchema),
+        defaultValues: {
+            tipo: 'read'
+        }
+    })
 
     async function onSubmit(data: Permiso){
         await createPermiso(data);
@@ -46,6 +48,7 @@ export default function PermisosForm() {
                 <SelectItem startContent={typeIcons["read"]} key={'read'}>Lectura</SelectItem>
                 <SelectItem startContent={typeIcons["write"]} key={'write'}>Escritura</SelectItem>
                 <SelectItem startContent={typeIcons["update"]} key={'update'}>Actualización</SelectItem>
+                <SelectItem startContent={typeIcons["delete"]} key={'delete'}>Deshabilitación</SelectItem>
             </Select>
             {errors.tipo && <ErrorMessage>{errors.tipo.message}</ErrorMessage>}
 
@@ -54,16 +57,6 @@ export default function PermisosForm() {
 
             <Textarea {...register("descripcion")} label="Descripción" />
             {errors.descripcion && <ErrorMessage>{errors.descripcion.message}</ErrorMessage>}
-
-            <Divider className="my-4" />
-
-            <p className="text-xl font-semibold">Ruta</p>
-
-            <Input {...register("rutaRuta")} label="Ruta" />
-            {errors.rutaRuta && <ErrorMessage>{errors.rutaRuta.message}</ErrorMessage>}
-
-            <Input {...register("rutaNombre")} label="Nombre" />
-            {errors.rutaNombre && <ErrorMessage>{errors.rutaNombre.message}</ErrorMessage>}
 
             <div className="flex ms-auto gap-4">
                 <Button type="button" color="danger" variant="light" onPress={onClose}>
--- src/types/modules/Permiso.ts
+++ src/types/modules/Permiso.ts
@@ -4,9 +4,7 @@ export const PermisoSchema = z.object({
     nombre : z.string().min(3,"Ingrese mínimo 3 caracteres"),
     descripcion : z.string().min(10,"Ingrese mínimo 10 caracteres"),
     tipo : z.string({required_error:"Ingrese un tipo"}).min(1,"Tipo requerido"),
-    moduloId : z.number({required_error:"Ingrese un módulo",invalid_type_error:"Ingrese un valor"}),
-    rutaRuta : z.string({required_error:"Ingrese una ruta"}).min(4,"Ingrese mínimo 4 caracteres"),
-    rutaNombre : z.string({required_error:"Ingrese un nombre para la ruta"}).min(4,"Ingrese mínimo 4 caracteres")
+    rutaId : z.number({required_error:"Ingrese un módulo",invalid_type_error:"Ingrese un valor"}),
 })
 
 export type Permiso = z.infer<typeof PermisoSchema>
