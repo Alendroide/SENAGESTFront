163320817b5bec30de5a12866189a0d6b83669bc
Alendroide <alendroideyt@gmail.com>
2025-06-04 07:44:07 -0500

    Permisos refetching successfully implemented

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -4,6 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import { Modulo, Permiso } from "@/types/default/Modulo";
 import { useContext, useEffect, useState, createContext } from "react";
 import Cookies from 'universal-cookie';
+import { axiosAPI } from "@/api/axiosAPI";
 
 const AuthContext = createContext<Auth | null>(null);
 export const AuthData = () => useContext(AuthContext) as Auth;
@@ -26,10 +27,14 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
       const permissions = cookies.get('permissions') || [];
     
       if(token) {
+
+        // Se decodifica el payload
+
         const payload = jwtDecode<JwtPayload>(token);
         
-        setIsAuthenticated(true);
+        // Se restauran las variables previas
 
+        setIsAuthenticated(true);
         setUser({
           sub : payload.sub,
           identificacion : payload.identificacion,
@@ -38,12 +43,29 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
           img : payload.img,
           rol : payload.rol ?? undefined
         });
-        
         setModules(modules);
-
         setPermissions(permissions);
-      }
-      setAppLoading(false);
+
+        // Se hace un refetching de los permisos
+
+        async function refetchPermisos(){
+          try{
+            const newPermisos = await axiosAPI.get('auth/getpermisos');
+            const modules = newPermisos.data.modulos;
+            const permissions = modules.flatMap((module: any) => module.rutas).flatMap((ruta: any) => ruta.permisos) || [];
+            setModules(modules);
+            setPermissions(permissions);
+          }
+          catch(error){
+            console.log(error)
+          }
+          finally{
+            setAppLoading(false);
+          }
+        }
+        refetchPermisos();
+
+      } else setAppLoading(false);
     },[])
 
     return (
