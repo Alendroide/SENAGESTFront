c04135a96bd1938109938077473d8664fcf24705
Alendroide <alendroideyt@gmail.com>
2025-06-09 08:33:12 -0500

    admin account can now login with no email

--- src/types/default/Login.ts
+++ src/types/default/Login.ts
@@ -2,16 +2,20 @@ import z from 'zod';
 import { Modulo } from './Modulo';
 
 export const LoginSchema = z.object({
-    correo : z.string().email("Ingrese un correo válido"),
-    contrasena : z.string().min(6,"La contraseña debe tener mínimo 6 caracteres")
+    correo: z.string().refine((val) => {
+        return val === 'admin' || z.string().email().safeParse(val).success;
+    }, {
+        message: 'Ingrese un correo válido',
+    }),
+    contrasena: z.string().min(6, "La contraseña debe tener mínimo 6 caracteres")
 })
 
 export type Login = z.infer<typeof LoginSchema>
 
 export type LoginResponse = {
-    access_token : string;
-    message : string;
-    status : number;
-    error : string | undefined;
-    modulos : Modulo[] | undefined
+    access_token: string;
+    message: string;
+    status: number;
+    error: string | undefined;
+    modulos: Modulo[] | undefined
 }
