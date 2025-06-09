4c860283ff69e8e59c64d7f3f9ced04199a40544
Alendroide <alendroideyt@gmail.com>
2025-06-09 08:00:14 -0500

    Style and rol permisos check fixed in profile page

--- src/pages/defaultPages/ProfilePage.tsx
+++ src/pages/defaultPages/ProfilePage.tsx
@@ -15,7 +15,7 @@ export default function ProfilePage() {
           <div className="flex flex-col lg:flex-row">
 
             {/* Profile */}
-            <div className="bg-gray-900 relative rounded-lg lg:w-1/3 lg:rounded-none lg:rounded-tl-lg lg:rounded-bl-lg">
+            <div className="bg-gray-900 relative rounded-lg lg:w-1/3 lg:rounded-none lg:rounded-tl-lg lg:rounded-bl-lg pb-4">
               <img
                 src={`${baseURL}uploads/${profile?.img}`}
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 h-36 rounded-full mx-auto border-2 border-dashed border-gray-500 shadow-lg"
@@ -59,7 +59,7 @@ export default function ProfilePage() {
 
             {/* Permisos */}
             <div className="bg-gray-50 rounded-lg lg:w-2/3 lg:rounded-none lg:rounded-tr-lg lg:rounded-br-lg">
-                {profile.rol ? profile.rol.permisos.length > 0 ? 
+                {profile.rol ? profile.rol.permisos?.length > 0 ?
                   <div className="p-6">
                     <h3 className="font-semibold my-4">Permisos asignados:</h3>
                     <div>
@@ -70,9 +70,9 @@ export default function ProfilePage() {
                     </div>
                   </div>
                   :
-                  <p className="font-semibold text-gray-600 text-center text-2xl">Sin permisos en el sistema!</p>
+                  <p className="font-semibold text-gray-600 text-center text-2xl mt-6">Sin permisos en el sistema!</p>
                   :
-                  <p className="font-semibold text-gray-600 text-center text-2xl">Sin rol asignado!</p>
+                  <p className="font-semibold text-gray-600 text-center text-2xl mt-6">Sin rol asignado!</p>
                 }
             </div>
 
