91fc8451e937dd354cda1055d1e2c80246e2561d
Alendroide <alendroideyt@gmail.com>
2025-06-17 10:33:41 -0500

    updateUser function added to useUsuario hook

--- src/hooks/default/useUsuario.ts
+++ src/hooks/default/useUsuario.ts
@@ -1,4 +1,5 @@
 import { axiosAPI } from "@/api/axiosAPI";
+import { UsuarioUpdate } from "@/types/modules/Usuario";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -45,6 +46,21 @@ export default function useUsuario(){
         }
     }
 
-    return { users, isLoading, isError, error, setPage, totalPages, createUser };
+    async function updateUser(id: number, data: UsuarioUpdate) {
+        try{
+            const response = await axiosAPI.patch(`usuarios/${id}`, data);
+            const updatedRecord = response.data.data;
+            queryClient.invalidateQueries({
+                queryKey: ["users"],
+                exact: false
+            });
+            return updatedRecord;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    return { users, isLoading, isError, error, setPage, totalPages, createUser, updateUser };
 
 }
