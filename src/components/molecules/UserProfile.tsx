11ae5e65b656aafb9b365569710cea607d56c0e9
Alendroide <alendroideyt@gmail.com>
2025-07-04 11:04:07 -0500

    UserProfile molecule updated to check for null values

--- src/components/molecules/UserProfile.tsx
+++ src/components/molecules/UserProfile.tsx
@@ -3,13 +3,13 @@ import { Auth } from "@/types/default/Auth"
 import { User } from "lucide-react";
 
 export default function UserProfile(){
-    const { user : { nombre, img }} = AuthData() as Auth;
+    const { user} = AuthData() as Auth;
     return(
         <div className="flex items-center">
-            { nombre ? <p>{nombre}</p> : <p>Usuario</p>}
+            { user?.nombre ? <p>{user.nombre}</p> : <p>Usuario</p>}
             
-            { img ?
-                <img width={'24px'} height={'24px'} src={`${import.meta.env.VITE_API_URL}uploads/${img}`} className="mx-2 rounded-full aspect-square object-cover" />
+            { user?.img ?
+                <img width={'24px'} height={'24px'} src={`${import.meta.env.VITE_API_URL}uploads/${user.img}`} className="mx-2 rounded-full aspect-square object-cover" />
             : 
                 <User className="mx-2" />
             }
